97302f417fda2a6b17893a4f976884ae59fa1e80
unknown <madcat182@gmail.com>
2016-08-17 12:38:59 -0500

    Updated screen company

--- BGAnimations/ScreenCompany decorations/default.lua
+++ BGAnimations/ScreenCompany decorations/default.lua
@@ -2,16 +2,18 @@ local t = Def.ActorFrame {};
 
 t[#t+1] = Def.ActorFrame {
 	LoadActor( "konami" )..{
-		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;);
+		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;diffusealpha,0;sleep,0.468;diffusealpha,1;sleep,4.571;linear,0.25;diffusealpha,0);
+	}
+};
+
+t[#t+1] = Def.ActorFrame {
+	LoadActor( "bemani" )..{
+		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;diffusealpha,0;sleep,5.106;linear,0.467;diffusealpha,1;);
 	}
 };
 
 t[#t+1] = Def.Quad{
-	InitCommand=cmd(Center;FullScreen;diffusecolor,color("#FFFFFF"););
+	InitCommand=cmd(Center;FullScreen;diffusecolor,color("#FFFFFF");linear,0.601;diffusealpha,0;);
 };
-
--- t[#t+1] = Def.ActorFrame {
--- 	SOUND:PlayAnnouncer("game over")
--- };
 
 return t
