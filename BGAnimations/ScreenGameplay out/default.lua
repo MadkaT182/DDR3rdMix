39806360c3836f9bdb68602f11a52a4ba407fac7
unknown <madcat182@gmail.com>
2016-08-17 12:39:27 -0500

    Prevent gameplay out in demonstration

--- BGAnimations/ScreenGameplay out/default.lua
+++ BGAnimations/ScreenGameplay out/default.lua
@@ -1,17 +1,21 @@
 local t = Def.ActorFrame {};
 
-t[#t+1] = Def.Quad{
-	InitCommand=cmd(Center;FullScreen;diffusecolor,color("#FFFFFF");diffusealpha,0;sleep,.3;linear,0.5;diffusealpha,1);
-};
+if not GAMESTATE:IsDemonstration() then
 
-t[#t+1] = Def.ActorFrame {
-	LoadActor( "../_doorclose" )..{}
-};
+	t[#t+1] = Def.Quad{
+		InitCommand=cmd(Center;FullScreen;diffusecolor,color("#FFFFFF");diffusealpha,0;sleep,.3;linear,0.5;diffusealpha,1);
+	};
 
-t[#t+1] = Def.ActorFrame {
-	LoadActor( "cleared" )..{
-		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y+2;sleep,2);
-	}
-};
+	t[#t+1] = Def.ActorFrame {
+		LoadActor( "../_doorclose" )..{}
+	};
+
+	t[#t+1] = Def.ActorFrame {
+		LoadActor( "cleared" )..{
+			OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y+2;sleep,2);
+		}
+	};
+
+end
 
 return t
