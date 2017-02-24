ac45fa8a06ca5f4187d9e3b1e67796625072a1b3
unknown <madcat182@gmail.com>
2017-02-24 11:16:37 -0500

    Added initial version of evaluation screen

--- BGAnimations/ScreenEvaluationNormal decorations/default.lua
+++ BGAnimations/ScreenEvaluationNormal decorations/default.lua
@@ -1,5 +1,6 @@
 local t = Def.ActorFrame {};
 
+--Title
 if GAMESTATE:IsCourseMode() then
 	t[#t+1] = Def.ActorFrame {
 		LoadActor( "nsr" )..{
@@ -13,6 +14,11 @@ else
 		}
 	};
 end;
+
+--Grade
+t[#t+1] = LoadActor("grade")..{
+	OffCommand=cmd(sleep,0.2;linear,0.2;diffusealpha,0);
+};
 
 --CD
 t[#t+1] = Def.ActorFrame {
@@ -28,6 +34,32 @@ if GAMESTATE:IsPlayerEnabled(PLAYER_1) then
 	t[#t+1] = LoadActor("statsP1")..{
 		OnCommand=cmd(x,SCREEN_CENTER_X-72;y,SCREEN_CENTER_Y-64)
 	};
+	--Song score p1
+	t[#t+1] = Def.RollingNumbers {
+		File = THEME:GetPathF("ScoreDisplayNormal", "Text");
+		InitCommand=cmd(x,SCREEN_CENTER_X-204;y,SCREEN_CENTER_Y+217;playcommand,"Set");
+		SetCommand = function (self)
+			local score = STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_1):GetScore();
+			self:Load("RollingNumbersEvaluation");
+			self:targetnumber(score);
+			self:sleep(1);
+			self:linear(1);
+			self:addy(-30);
+		end;
+	};
+	--Total score p1
+	t[#t+1] = Def.RollingNumbers {
+		File = THEME:GetPathF("ScoreDisplayTotal", "Text");
+		InitCommand=cmd(x,SCREEN_CENTER_X-204;y,SCREEN_CENTER_Y+257;playcommand,"Set");
+		SetCommand = function (self)
+			local score = STATSMAN:GetAccumPlayedStageStats():GetPlayerStageStats(PLAYER_1):GetScore();
+			self:Load("RollingNumbersEvaluation");
+			self:targetnumber(score);
+			self:sleep(1);
+			self:linear(1);
+			self:addy(-40);
+		end;
+	};
 end
 
 if GAMESTATE:IsPlayerEnabled(PLAYER_2) then
@@ -35,6 +67,33 @@ if GAMESTATE:IsPlayerEnabled(PLAYER_2) then
 	t[#t+1] = LoadActor("statsP2")..{
 		OnCommand=cmd(x,SCREEN_CENTER_X+131;y,SCREEN_CENTER_Y-64)
 	};
+	--Song score p1
+	t[#t+1] = Def.RollingNumbers {
+		File = THEME:GetPathF("ScoreDisplayNormal", "Text");
+		InitCommand=cmd(x,SCREEN_CENTER_X+204;y,SCREEN_CENTER_Y+217;playcommand,"Set");
+		SetCommand = function (self)
+			local score = STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_2):GetScore();
+			self:Load("RollingNumbersEvaluation");
+			self:targetnumber(score);
+			self:sleep(1);
+			self:linear(1);
+			self:addy(-30);
+		end;
+	};
+	--Total score p1
+	t[#t+1] = Def.RollingNumbers {
+		File = THEME:GetPathF("ScoreDisplayTotal", "Text");
+		InitCommand=cmd(x,SCREEN_CENTER_X+204;y,SCREEN_CENTER_Y+257;playcommand,"Set");
+		SetCommand = function (self)
+			local score = STATSMAN:GetAccumPlayedStageStats():GetPlayerStageStats(PLAYER_2):GetScore();
+			self:Load("RollingNumbersEvaluation");
+			self:targetnumber(score);
+			self:sleep(1);
+			self:linear(1);
+			self:addy(-40);
+		end;
+	};
 end
+
 
 return t
