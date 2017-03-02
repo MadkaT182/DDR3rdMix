e70f71f36622614b843348c7133f584efa68b7f6
unknown <madcat182@gmail.com>
2017-03-02 11:06:53 -0500

    Updated Play mode decorations

---
+++ BGAnimations/ScreenRanking decorations/default.lua
@@ -0,0 +1,7 @@
+return Def.ActorFrame {
+
+LoadActor("title")..{
+	OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-220);
+};
+
+}
---
+++ BGAnimations/ScreenSelectPlayMode decorations/default.lua
@@ -0,0 +1,5 @@
+return Def.ActorFrame {
+	LoadActor( "title" )..{
+		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_TOP+47);
+	};
+}
