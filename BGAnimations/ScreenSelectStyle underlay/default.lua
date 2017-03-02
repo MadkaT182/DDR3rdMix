0018d219f80d32c350d04b8bccdc9c0e0e163896
unknown <madcat182@gmail.com>
2017-03-02 11:04:53 -0500

    Updated select style underlay

--- BGAnimations/ScreenSelectStyle underlay/default.lua
+++ BGAnimations/ScreenSelectStyle underlay/default.lua
@@ -7,6 +7,29 @@ t[#t+1] = Def.ActorFrame {
 	LoadActor( "title" )..{
 		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y-69);
 	};
+--Dancers
+	LoadActor("../ScreenTitleMenu background/md")..{
+		InitCommand=cmd(Center;addx,-200;addy,10;zoom,1.81;diffuse,.4,.4,1,2;playcommand,"Set");
+		SetCommand=function (self)
+			if GAMESTATE:IsSideJoined(PLAYER_1) then
+				self:diffusealpha(1);
+			else
+				self:diffusealpha(0);
+			end;
+		end;
+		--SystemMessageMessageCommand=cmd(playcommand,"Set");
+	};
+	LoadActor("../ScreenTitleMenu background/fd")..{
+		InitCommand=cmd(Center;addx,200;addy,10;zoom,1.81;diffuse,.95,.8,.7,.65;playcommand,"Set");
+		SetCommand=function (self)
+			if GAMESTATE:IsSideJoined(PLAYER_2) then
+				self:diffusealpha(1);
+			else
+				self:diffusealpha(0);
+			end;
+		end;
+		--SystemMessageMessageCommand=cmd(playcommand,"Set");
+	};
 };
 
 return t
