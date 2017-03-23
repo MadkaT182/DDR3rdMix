b41f1756ae06e06b3b08062fbb0eddfaf17335d9
unknown <madcat182@gmail.com>
2017-03-23 15:39:17 -0500

    Updated course mode

--- BGAnimations/ScreenSelectMusic overlay.lua
+++ BGAnimations/ScreenSelectMusic overlay.lua
@@ -1,16 +1,18 @@
 local t = Def.ActorFrame {};
 
---PlayMode
-t[#t+1] = Def.ActorFrame {
-	LoadActor( "_style" )..{
-		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_TOP+82)
-	}
-};
+if not GAMESTATE:IsCourseMode() then
+	--PlayMode
+	t[#t+1] = Def.ActorFrame {
+		LoadActor( "_style" )..{
+			OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_TOP+82);
+		}
+	};
+end;
 
 --Player Indicator
 t[#t+1] = Def.ActorFrame {
 	LoadActor("Ind1p")..{
-		InitCommand=cmd(x,SCREEN_LEFT+147;y,SCREEN_BOTTOM-190;)
+		InitCommand=cmd(x,SCREEN_LEFT+147;y,SCREEN_BOTTOM-190);
 	};
 	LoadActor("Ind2p")..{
 		InitCommand=cmd(x,SCREEN_RIGHT-147;y,SCREEN_BOTTOM-190;);
