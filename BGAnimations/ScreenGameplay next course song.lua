7d51b3577e9ebc47a6f891dddaa8a34e0a525415
unknown <madcat182@gmail.com>
2017-03-25 11:13:51 -0500

    Updated nextcourse song animation

--- BGAnimations/ScreenGameplay next course song.lua
+++ BGAnimations/ScreenGameplay next course song.lua
@@ -25,7 +25,8 @@ t[#t+1] = Def.ActorFrame {
 
 --Stage frame
 t[#t+1] = LoadActor("_stageFrame")..{
-	OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_TOP+54);
+	StartCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_TOP+54);
+	FinishCommand=cmd(decelerate,0.5;diffusealpha,0);
 };
 
 --Score frame
@@ -37,12 +38,14 @@ t[#t+1] = LoadActor("_stageFrame")..{
 --Play mode
 t[#t+1] = Def.ActorFrame {
 	LoadActor( "_style" )..{
-		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_BOTTOM-72)
+		StartCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_BOTTOM-72;diffusealpha,1);
+		FinishCommand=cmd(decelerate,0.5;diffusealpha,0);
 	}
 };
 t[#t+1] = Def.ActorFrame {
 	LoadActor( "_playmode" )..{
-		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_BOTTOM)
+		StartCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_BOTTOM;diffusealpha,1);
+		FinishCommand=cmd(decelerate,0.5;diffusealpha,0);
 	}
 };
 
