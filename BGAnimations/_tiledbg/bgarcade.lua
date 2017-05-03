fed78bc304d09c8fc9e3c3dc256e74703b0f2805
unknown <madcat182@gmail.com>
2017-05-03 09:35:20 -0500

    Updated tiledbg behaviour

---
+++ BGAnimations/_tiledbg/bgarcade.lua
@@ -0,0 +1,12 @@
+local t = Def.ActorFrame{
+	LoadActor("arcade")..{
+		InitCommand=cmd(FullScreen;);
+		OnCommand=function(self)
+			local w = DISPLAY:GetDisplayWidth() / self:GetWidth();
+			local h = DISPLAY:GetDisplayHeight() / self:GetHeight();
+			self:customtexturerect(0,0,w,h);
+			self:texcoordvelocity(0.5,-0.5);
+		end;
+	};
+}
+return t;
--- BGAnimations/_tiledbg/default.lua
+++ BGAnimations/_tiledbg/default.lua
@@ -1,12 +1,9 @@
 local t = Def.ActorFrame{
-	LoadActor("bgtile")..{
-		InitCommand=cmd(FullScreen;);
-		OnCommand=function(self)
-			local w = DISPLAY:GetDisplayWidth() / self:GetWidth();
-			local h = DISPLAY:GetDisplayHeight() / self:GetHeight();
-			self:customtexturerect(0,0,w,h);
-			self:texcoordvelocity(0.5,-0.5);
-		end;
+	LoadActor("bgarcade")..{
+		Condition=GAMESTATE:GetCoinMode() == 'CoinMode_Pay';
+	};
+	LoadActor("bghome")..{
+		Condition=GAMESTATE:GetCoinMode() ~= 'CoinMode_Pay';
 	};
 }
 return t;
