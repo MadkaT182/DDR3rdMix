ec99d3b9c79e4f77cebadf7464599f1edc7ef5af
unknown <madcat182@gmail.com>
2017-08-24 11:11:19 -0500

    Updated system layer

--- BGAnimations/ScreenSystemLayer overlay.lua
+++ BGAnimations/ScreenSystemLayer overlay.lua
@@ -8,7 +8,7 @@ local function CreditsText(playr)
 	return LoadFont("ScreenSystemLayer credits normal")..{
 		InitCommand=cmd(x,posX;y,SCREEN_BOTTOM-16;playcommand,"Refresh");
 		RefreshCommand=function(self)
-			if GAMESTATE:GetCoinMode() == 'CoinMode_Free' then
+			if GAMESTATE:GetCoinMode() == 'CoinMode_Free' or GAMESTATE:GetCoinMode() == 'CoinMode_Home' then
 				self:diffusealpha(0);
 			elseif GAMESTATE:IsEventMode() then
 				self:diffusealpha(0);
@@ -35,6 +35,7 @@ local function CreditsText(playr)
 			end
 		end;
 		CoinInsertedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		CoinModeChangedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
 		RefreshCreditTextMessageCommand=cmd(stoptweening;playcommand,"Refresh");
 		PlayerJoinedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
 		ScreenChangedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
@@ -57,6 +58,7 @@ local function OtherText()
 			end
 		end;
 		CoinInsertedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
+		CoinModeChangedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
 		RefreshCreditTextMessageCommand=cmd(stoptweening;playcommand,"Refresh");
 		PlayerJoinedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
 		ScreenChangedMessageCommand=cmd(stoptweening;playcommand,"Refresh");
@@ -69,6 +71,45 @@ t[#t+1] = Def.ActorFrame {
  	CreditsText( 'PLAYER_1' );
 	CreditsText( 'PLAYER_2' );
 	OtherText();
+	ScreenChangedMessageCommand=function(self)
+			self:visible(THEME:GetMetric(SCREENMAN:GetTopScreen():GetName(),"ShowCreditDisplay"));
+	end;
 };
+
+-- SystemMessage Text
+t[#t+1] = Def.ActorFrame {
+	SystemMessageMessageCommand=function(self, params)
+		SystemMessageText:settext( params.Message )
+		self:playcommand( "On" )
+		if params.NoAnimate then
+			self:finishtweening()
+		end
+		self:playcommand( "Off" )
+	end,
+	HideSystemMessageMessageCommand=cmd(finishtweening),
+
+	Def.Quad {
+		InitCommand=function(self)
+			self:zoomto(_screen.w, 30):horizalign(left):vertalign(top)
+				:diffuse(Color.Black):diffusealpha(0)
+		end,
+		OnCommand=function(self)
+			self:finishtweening():diffusealpha(0.85)
+				:zoomto(_screen.w, (SystemMessageText:GetHeight() + 16) * 0.8 )
+		end,
+		OffCommand=function(self) self:sleep(3):linear(0.5):diffusealpha(0) end,
+	},
+
+	LoadFont("Common normal")..{
+		Name="Text",
+		InitCommand=function(self)
+			self:maxwidth(750):horizalign(left):vertalign(top)
+				:xy(SCREEN_LEFT+10, 10):diffusealpha(0):zoom(0.5)
+			SystemMessageText = self
+		end,
+		OnCommand=function(self) self:finishtweening():diffusealpha(1) end,
+		OffCommand=function(self) self:sleep(3):linear(0.5):diffusealpha(0) end,
+	}
+}
 
 return t;
