3207dd961eaf987b95f322c7d59a3e66e1e0762c
unknown <madcat182@gmail.com>
2020-04-21 22:12:19 -0500

    General update

--- BGAnimations/ScreenSelectPlayMode decorations/default.lua
+++ BGAnimations/ScreenSelectPlayMode decorations/default.lua
@@ -1,5 +1,9 @@
 return Def.ActorFrame {
-	LoadActor( "title" )..{
-		OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_TOP+47);
+	LoadActor("title")..{
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_TOP+47);
+		OnCommand=cmd(addx,43;diffusealpha,0;linear,.166;addx,-43;diffusealpha,1);
+	};
+	LoadActor(THEME:GetPathG("Common",IsHome() and "ui/selst_cs" or "ui/selst_ac"))..{
+		OnCommand=cmd(x,SCREEN_CENTER_X-219;y,SCREEN_CENTER_Y-86;diffuseblink);
 	};
 }
