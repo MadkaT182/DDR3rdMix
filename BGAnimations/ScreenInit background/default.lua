bc1f83d661d6509648e5221da88ee5f6dfd22a99
unknown <madcat182@gmail.com>
2020-04-21 22:33:33 -0500

    Fixing graphics for widescreen

--- BGAnimations/ScreenInit background/default.lua
+++ BGAnimations/ScreenInit background/default.lua
@@ -15,25 +15,19 @@ return Def.ActorFrame {
 		}
 	};
 	LoadActor("boot2")..{
-		InitCommand=cmd(FullScreen;diffusealpha,0;sleep,1.4;diffusealpha,1);
+		InitCommand=cmd(Center;diffusealpha,0;sleep,1.4;diffusealpha,1);
 	};
 	Def.ActorFrame {
 		InitCommand=cmd(diffusealpha,0;sleep,2.16;diffusealpha,1);
-		Def.Quad{
-			InitCommand=cmd(FullScreen;diffuse,color("#294A4A"););
-		};
 		LoadActor("boot3")..{
 			InitCommand=cmd(Center);
 		}
 	};
 	LoadActor("boot2")..{
-		InitCommand=cmd(FullScreen;diffusealpha,0;sleep,2.36;diffusealpha,1);
+		InitCommand=cmd(Center;diffusealpha,0;sleep,2.36;diffusealpha,1);
 	};
 	Def.ActorFrame {
 		InitCommand=cmd(diffusealpha,0;sleep,2.16;diffusealpha,1);
-		Def.Quad{
-			InitCommand=cmd(FullScreen;diffuse,color("#294A4A"););
-		};
 		LoadActor("boot4")..{
 			InitCommand=cmd(Center);
 		}
