6868e74c9252eb1f1d0a88c05af1e948fe9132a1
unknown <madcat182@gmail.com>
2020-04-22 16:08:13 -0500

    Update revamp/course decorations

--- BGAnimations/ScreenSelectCourse decorations/default.lua
+++ BGAnimations/ScreenSelectCourse decorations/default.lua
@@ -1,30 +1,28 @@
-local t = Def.ActorFrame {};
---Course Name
-t[#t+1] = LoadFont("_nonstop") .. {
-	InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_BOTTOM-92;maxwidth,SCREEN_WIDTH);
-	SetCommand=function(self)
-		local course = GAMESTATE:GetCurrentCourse();
-		if course then
-			if GroupNameChange then
-				self:settext(ChangeGroupName(course:GetDisplayFullTitle()));
-			else
-				self:settext(course:GetDisplayFullTitle());
+return Def.ActorFrame {
+	LoadFont("_nonstop") .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_BOTTOM-92;maxwidth,SCREEN_WIDTH);
+		SetCommand=function(self)
+			local course = GAMESTATE:GetCurrentCourse();
+			if course then
+				if GroupNameChange then
+					self:settext(ChangeGroupName(course:GetDisplayFullTitle()));
+				else
+					self:settext(course:GetDisplayFullTitle());
+				end;
 			end;
 		end;
-	end;
-	CurrentCourseChangedMessageCommand=cmd(playcommand,"Set");
-	OffCommand=cmd();
+		CurrentCourseChangedMessageCommand=cmd(playcommand,"Set");
+	};
+	LoadActor("courselist")..{
+		OnCommand=cmd(x,SCREEN_CENTER_X-304;y,SCREEN_CENTER_Y);
+	};
+	LoadActor("selcours")..{
+		OnCommand=cmd(x,SCREEN_CENTER_X-2;y,SCREEN_CENTER_Y-216);
+	};
+	LoadActor("../Ind1p")..{
+		OnCommand=cmd(x,SCREEN_LEFT+167;y,SCREEN_BOTTOM-84;)
+	};
+	LoadActor("../Ind2p")..{
+		OnCommand=cmd(x,SCREEN_RIGHT-167;y,SCREEN_BOTTOM-84;);
+	};
 };
-
---Course Contents
-t[#t+1] = LoadActor("courselist")..{
-	OnCommand=cmd(x,SCREEN_CENTER_X-304;y,SCREEN_CENTER_Y);
-};
-
-t[#t+1] = Def.ActorFrame {
-	LoadActor( "selcours" )..{
-		OnCommand=cmd(Center;addx,-2;addy,-216);
-	}
-};
-
-return t
