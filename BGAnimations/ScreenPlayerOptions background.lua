2891e7fca28b07f1bdc6ba05b52cb9d914b2b1e4
unknown <madcat182@gmail.com>
2020-04-24 18:41:42 -0500

    General update

---
+++ BGAnimations/ScreenPlayerOptions background.lua
@@ -0,0 +1,3 @@
+return Def.ActorFrame {
+	LoadActor("ScreenOptionsHome background");
+};
--- BGAnimations/_doorclose/delay.lua
+++ BGAnimations/_doorclose/delay.lua
@@ -1,25 +1,24 @@
 local t = Def.ActorFrame {};
-local delay = 1;
 
 --Left Door
 t[#t+1] = Def.ActorFrame {
 	LoadActor("../doorbg")..{
-		OnCommand=cmd(FullScreen;cropright,0.5;x,SCREEN_LEFT;sleep,delay;linear,0.25;addx,SCREEN_WIDTH/2);
+		OnCommand=cmd(FullScreen;cropright,0.5;x,SCREEN_LEFT;sleep,1;linear,0.25;addx,SCREEN_WIDTH/2);
 	}
 };
 
 --Right Door
 t[#t+1] = Def.ActorFrame {
 	LoadActor("../doorbg")..{
-		OnCommand=cmd(FullScreen;cropleft,0.5;x,SCREEN_RIGHT;sleep,delay;linear,0.25;addx,-SCREEN_WIDTH/2);
+		OnCommand=cmd(FullScreen;cropleft,0.5;x,SCREEN_RIGHT;sleep,1;linear,0.25;addx,-SCREEN_WIDTH/2);
 	}
 };
 
 --Sound
 t[#t+1] = Def.ActorFrame {
 	LoadActor("close")..{
-		StartTransitioningCommand=cmd(sleep,delay;queuecommand,"play");
-		PlayCommand=cmd(play);
+		StartTransitioningCommand=cmd(sleep,1;queuecommand,"Snd");
+		SndCommand=cmd(play);
 	};
 };
 
